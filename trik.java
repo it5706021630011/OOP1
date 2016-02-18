0cecf0d874b941c4f60e9cc795a9738aa0b06ed3
it5706021630011 <5706021630011@fitm.kmutnb.ac.th>
2016-02-18 19:47:29 +0700

    Update trik.java

--- trik.java
+++ trik.java
@@ -1,32 +1,40 @@
 import java.io.*;
 import java.util.Scanner;
 public class trik {
+static	String input ;
+static	int a=1,b=0,c=0,i=0,tmp;
 public static void main(String args[]){
-	String input ;
-	int a=1,b=0,c=0,i=0,tmp;
 	char t ;
 	Scanner scan = new Scanner (System.in);
 	System.out.print("Enter :");
 	input = scan.nextLine().toUpperCase();
 	while(i!=input.length()){
 		t=input.charAt(i);
-		if(t=='A'){
-			tmp=a;
-			a=b;
-			b=tmp;
-		}
-		else if (t=='B'){
-			tmp=b;
-			b=c;
-			c=tmp;
-		}
-		else if (t=='C'){
-			tmp=a;
-			a=c;
-			c=tmp;
-		}
+		tInput(t);
+		
 		i++;
 	}
+	resaltP(a,b,c);
+	
+}
+public static void tInput(char t){
+	if(t=='A'){
+		tmp=a;
+		a=b;
+		b=tmp;
+	}
+	else if (t=='B'){
+		tmp=b;
+		b=c;
+		c=tmp;
+	}
+	else if (t=='C'){
+		tmp=a;
+		a=c;
+		c=tmp;
+	}
+}
+public static void resaltP(int a,int b,int c){
 	if(a==1){
 		System.out.println("ball in glass 1");
 	}
