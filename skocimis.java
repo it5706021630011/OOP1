afeb55c2446a20296deef65c9a236e4711fc3b23
it5706021630011 <5706021630011@fitm.kmutnb.ac.th>
2016-02-18 21:51:56 +0700

    Update skocimis.java

--- skocimis.java
+++ skocimis.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
 public class Skocimis {
 	public static void main(String[] args) {
-		int n1, n2, n3;
+		int n1, n2, n3,i=0;
 		Scanner scan = new Scanner(System.in);
-		do {
+		while(i<3) {
 			System.out.print("Enter 3 integer (0 < Number1 < Number2 < Number3 < 100): ");
-			n1 = scan.nextInt();
-			n2 = scan.nextInt();
-			n3 = scan.nextInt();
+			n1 = scan.nextInt();i++;
+			n2 = scan.nextInt();i++;
+			n3 = scan.nextInt();i++;
+			Add(n1, n2, n3,i);
+		}
+	}
+	public static void Add(int n1, int n2, int n3,int i){
+		while (n1 <= 0 || n1 >= 100 || n2 <= 0 || n2 >= 100 || n3 <= 0 || n3 >= 100 || n3 < n1 || n3 < n2 || n2 < n1
+				|| n1 > n2 || n1 > n3);{
+			if(i<3){
 			if (n1 <= 0 || n1 >= 100) {
 				System.out.println("Invalid Input. Please enter again!!!");
 			} else if (n2 <= 0 || n2 >= 100) {
@@ -19,9 +26,10 @@ public class Skocimis {
 			} else if (n3 <= n2) {
 				System.out.println("Number3 must more than Number2. Please enter again!!!");
 			}
-		} while (n1 <= 0 || n1 >= 100 || n2 <= 0 || n2 >= 100 || n3 <= 0 || n3 >= 100 || n3 < n1 || n3 < n2 || n2 < n1
-				|| n1 > n2 || n1 > n3);
-		System.out.println("----------------- Output ----------------------- ");
+			}
+		
+		}
+		System.out.println("------- Output -------- ");
 		System.out.println(" Most played : " + ((n3 - n2) - 1) + " time.");
 	}
 }
